6a56352a63b9213f6aaad9648591cf3ca6056a0c
Bartek <bartekduda01@gmail.com>
2024-12-19 00:24:50 +0100

    feat: just printing version now

--- main.go
+++ main.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"net/http"
-)
-
 func main() {
 	const version = "0.0.1"
-
-	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(version))
-	})
-
-	http.ListenAndServe(":8080", nil)
+	println(version)
 }
