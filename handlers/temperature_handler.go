88075b5591d53252d864a46531a110b30dd3d22e
Bartek <bartekduda01@gmail.com>
2024-12-21 20:29:37 +0100

    feat: get actual temperature from api

--- handlers/temperature_handler.go
+++ handlers/temperature_handler.go
@@ -1,9 +1,80 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
+type WeatherData struct {
+	Temperature float64 `json:"temp_c"`
+}
+
 func TemperatureHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Temperature"))
+	loc := r.URL.Query().Get("loc")
+	if len(loc) == 0 {
+		w.Write([]byte("loc query param is required"))
+		return
+	}
+
+	temp, err := getTemperature(loc)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte(strconv.FormatFloat(temp, 'f', -1, 64)))
+}
+
+func getTemperature(loc string) (float64, error) {
+	apiKey, err := getApiKey()
+	if err != nil {
+		return 0.0, err
+	}
+
+	url := "https://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + loc
+
+	response, err := http.Get(url)
+	if err != nil {
+		return 0.0, err
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return 0.0, err
+	}
+	defer response.Body.Close()
+
+	var result struct {
+		Current WeatherData `json:"current"`
+	}
+
+	json.Unmarshal(body, &result)
+
+	if result.Current.Temperature == 0 {
+		fmt.Println("temp was zero")
+		return 0.0, nil
+	}
+
+	return result.Current.Temperature, nil
+}
+
+func getApiKey() (string, error) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		return "", err
+	}
+
+	apiKey := os.Getenv("API_KEY")
+	if len(apiKey) == 0 {
+		return "", errors.New("API_KEY is not set")
+	}
+
+	return apiKey, nil
 }
--- handlers/temperature_handler_test.go
+++ handlers/temperature_handler_test.go
@@ -3,10 +3,32 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
 func TestTemperatureHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/temperature?loc=warsaw", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	TemperatureHandler(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			rr.Code, http.StatusOK)
+	}
+
+	body := rr.Body.String()
+	_, err = strconv.ParseFloat(body, 64)
+	if err != nil {
+		t.Errorf("not a float value in the body: %v", body)
+	}
+}
+
+func TestLocParamNotProvided(t *testing.T) {
 	req, err := http.NewRequest("GET", "/temperature", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -20,8 +42,8 @@ func TestTemperatureHandler(t *testing.T) {
 			rr.Code, http.StatusOK)
 	}
 
-	if rr.Body.String() != "Temperature" {
+	if rr.Body.String() != "loc query param is required" {
 		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), "Temperature")
+			rr.Body.String(), "loc query param is required")
 	}
 }
